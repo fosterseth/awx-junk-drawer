4d8a3d0e702116353c94c70428ba8e037f4e3a47
Seth Foster <fosterbseth@gmail.com>
2022-02-09 18:23:22 -0500

    profiling to collector benchmark

--- benchmark_collector.py
+++ benchmark_collector.py
@@ -1,9 +1,10 @@
 from django.db.models.deletion import Collector
-from awx.main.signals import disable_activity_stream, disable_computed_fields
+from awx.main.signals import disable_activity_stream, disable_computed_fields, pre_save, post_save, pre_delete, post_delete, m2m_changed
 import time
 from awx.main.utils.deletion import AWXCollector
 from django.utils.timezone import now
 import datetime
+import cProfile
 
 # moved firehose script into utils for easier import
 from awx.main.utils.firehose2 import generate_jobs
@@ -13,7 +14,7 @@ days_ago = 1
 num_jobs = 1000
 
 time_created = now() - datetime.timedelta(days=days_ago)
-
+prof = True
 
 def timeit(f):
     def wrapper(*args, **kwargs):
@@ -27,16 +28,28 @@ def timeit(f):
 @timeit
 def delete_jobs(collector):
     with disable_activity_stream(), disable_computed_fields():
-      collector.collect(Job.objects.all())
-      collector.delete()
+        qs = Job.objects.all()
+        collector.collect(qs)
+        collector.delete()
+
+for s in (pre_save, post_save, pre_delete, post_delete, m2m_changed):
+    with s.lock:
+        del s.receivers[:]
+        s.sender_receivers_cache.clear()
 
 print("AWXCollector")
 generate_jobs(num_jobs, time_created)
 ac = AWXCollector('default')
-delete_jobs(ac)
+if prof:
+    cProfile.run("delete_jobs(ac)", filename="AWXCollector.prof")
+else:
+    delete_jobs(ac)
 
 
 print("Collector")
 generate_jobs(num_jobs, time_created)
 oc = Collector('default')
-delete_jobs(oc)
+if prof:
+    cProfile.run("delete_jobs(oc)", filename="Collector.prof")
+else:
+    delete_jobs(oc)
